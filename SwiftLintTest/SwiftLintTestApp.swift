5fe7589dbbf739bc745b4c262dc469e917d86c50
sasasan03 <sako.hiro62@gmail.com>
2023-12-16 22:02:22 +0900

    first commit

---
+++ SwiftLintTest/SwiftLintTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLintTestApp.swift
+//  SwiftLintTest
+//
+//  Created by sako0602 on 2023/12/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLintTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
